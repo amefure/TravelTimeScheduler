17137320c3cc54314a22f5d1aee9e2cc15997482
ame <shachi10969@gmail.com>
2023-03-31 06:13:53 +0900

    Initial Commit

---
+++ TravelTimeScheduler/TravelTimeSchedulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelTimeSchedulerApp.swift
+//  TravelTimeScheduler
+//
+//  Created by t&a on 2023/03/31.
+//
+
+import SwiftUI
+
+@main
+struct TravelTimeSchedulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
